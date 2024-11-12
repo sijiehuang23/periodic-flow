14bc61f0c3d4e4479698a20e5ca3ebe313735c15
sjh <hsijie94@gmail.com>
2024-11-12 13:31:21 -0700

    simplify organization

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='periodicflow',
     version='0.1',
+    author='Sijie Huang',
     description="A simple pseudo-spectral Fourier-Galerkin solver for incompressible Navier-Stokes equations",
     package_dir={'': 'src'},
     packages=find_packages(where='src')
