dcd9889e9d94879402950059acb48c422c45d92d
sjh <hsijie94@gmail.com>
2024-11-18 19:02:13 -0700

    add pyproject.toml for build system configuration; update .gitignore to include egg-info files; remove version from setup.py; create egg-info directory with metadata

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 
 setup(
     name='periodicflow',
-    version='0.1',
     author='Sijie Huang',
     description="A simple pseudo-spectral Fourier-Galerkin solver for incompressible Navier-Stokes equations",
     package_dir={'': 'src'},
